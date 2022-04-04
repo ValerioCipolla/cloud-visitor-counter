b2bdebf88a7a409f5d814624b19ef6e8efede118
valeriocipolla <valeriocipolla92@gmail.com>
2022-04-04 15:35:32 +0100

    created fetch

--- frontend/main.js
+++ frontend/main.js
@@ -1,2 +1,12 @@
 const counter = document.getElementById("counter");
 counter.textContent = 1;
+
+async function fetchData() {
+  const res = await fetch(
+    `https://rci0sg2k5i.execute-api.eu-west-2.amazonaws.com/`
+  );
+  const data = await res.json();
+  console.log(data);
+}
+
+fetchData();
